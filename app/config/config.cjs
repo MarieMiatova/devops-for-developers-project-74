287807a831c2202586884db20f6c6d1341e0b0c9
MarieMiatova <146247021+MarieMiatova@users.noreply.github.com>
2026-04-04 18:07:06 +0300

    errors with hexlet check

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -2,12 +2,8 @@ require('dotenv').config();
 
 const config = {
   development: {
-    dialect: 'postgres',
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT || 5432,
+    dialect: 'sqlite',
+    storage: './database.sqlite',
   },
   test: {
     dialect: 'postgres',
